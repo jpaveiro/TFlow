cc9290bb164294cdadc3485ba2b2c02cda85bd17
jpaveiro <jpaveiros@gmail.com>
2025-07-05 10:47:29 -0300

    feat: adicionado validação de mínimo e máximo no atributo name para registro de usuário

--- src/main/java/com/github/jpaveiro/tflow/presentation/controllers/user/register_new_user/io/RegisterNewUserRequest.java
+++ src/main/java/com/github/jpaveiro/tflow/presentation/controllers/user/register_new_user/io/RegisterNewUserRequest.java
@@ -2,9 +2,12 @@ package com.github.jpaveiro.tflow.presentation.controllers.user.register_new_use
 
 import com.github.jpaveiro.tflow.presentation.dto.IRequest;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 
 public record RegisterNewUserRequest(
-        @NotBlank String name,
-        @Email String email) implements IRequest {
+        @NotBlank @Min(6) @Max(60) String name,
+        @Email String email
+) implements IRequest {
 }
